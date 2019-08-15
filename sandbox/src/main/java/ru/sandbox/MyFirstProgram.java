eb7bc17eeca77f67ebe554edaa6d518cc5435e26
lapatskiy <val@imastertek.com>
2019-08-14 20:17:17 -0700

    Revert

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -5,5 +5,21 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World Testing if anything has changed");
 
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println(2 + "2");
+
+        System.out.println((2 + 2) * 2);
+
+        System.out.println("2 + 2 = " + (2 + 2));
+
     }
 }
