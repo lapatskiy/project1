9de06f5340ec4693e6a78e2578c2d1bbae50a19a
lapatskiy <val@imastertek.com>
2019-08-21 14:13:36 -0700

    Brought Code to Correction

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -1,110 +1,24 @@
 package ru.sandbox;
 
 public class MyFirstProgram {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     public static void main(String[] args) {
         hello("world");
         hello("user");
         hello("Val");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Square s = new Square(5);
 
+        Square s = new Square(5);
         System.out.println("Distance of a square with a side " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
-
-
         System.out.println("Distance of a rectangle with a side " + r.a + " and " + r.b + " = " + r.area());
-
-=======
-        double l = 5;
-        System.out.println("Distance of a square with a side " + l + " = " + area(l));
-=======
-        Square s = new Square(5);
->>>>>>> parent of 7217d9f... Revert "Added test annotation"
-
-        System.out.println("Distance of a square with a side " + s.l + " = " + s.area());
-
-        Rectangle r = new Rectangle(4, 6);
-
-
-        System.out.println("Distance of a rectangle with a side " + r.a + " and " + r.b + " = " + r.area());
-=======
-
-    public static void main(String[] args) {
-        System.out.println("Hello World Testing if anything has changed");
->>>>>>> parent of cdfb7a0... Added additional functions
-
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
-        System.out.println(1 / 2.0);
-        System.out.println(2.0 / 2);
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-        System.out.println(2 + "2");
-
-        System.out.println((2 + 2) * 2);
-
-<<<<<<< HEAD
-    public static void hello (String somebody) {
-        System.out.println("hello, " + somebody + "!");
-    }
-<<<<<<< HEAD
-    public static double area(double len) {
-        return len * len;
-    }
->>>>>>> parent of 4120dcd... Revert "Added additional functions"
-=======
-        System.out.println("2 + 2 = " + (2 + 2));
->>>>>>> parent of cdfb7a0... Added additional functions
-
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    public static void hello (String somebody) {
-        System.out.println("hello, " + somebody + "!");
     }
 
-
-=======
->>>>>>> parent of 4120dcd... Revert "Added additional functions"
-=======
-
-
->>>>>>> parent of 7217d9f... Revert "Added test annotation"
-}
-=======
-}
->>>>>>> parent of cdfb7a0... Added additional functions
-=======
-
-    public static void main(String[] args) {
-        System.out.println("Hello World Testing if anything has changed");
-
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
-        System.out.println(1 / 2.0);
-        System.out.println(2.0 / 2);
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-        System.out.println(2 + "2");
-
-        System.out.println((2 + 2) * 2);
-
-        System.out.println("2 + 2 = " + (2 + 2));
-
+    private static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(Rectangle r) {
+        return r.a * r.b;
     }
 }
->>>>>>> parent of cdfb7a0... Added additional functions
--- sandbox/src/main/java/ru/sandbox/Square.java
+++ sandbox/src/main/java/ru/sandbox/Square.java
@@ -3,21 +3,15 @@ package ru.sandbox;
 public class Square {
     public double l;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 7217d9f... Revert "Added test annotation"
-    public Square (double l) {
+
+    public Square(double l) {
+
         this.l = l;
     }
 
     public double area() {
         return this.l * this.l;
     }
-
-<<<<<<< HEAD
-=======
->>>>>>> parent of 4120dcd... Revert "Added additional functions"
-=======
->>>>>>> parent of 7217d9f... Revert "Added test annotation"
 }
+
+
