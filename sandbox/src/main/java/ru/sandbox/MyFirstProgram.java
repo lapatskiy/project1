c0532544c199f3d105da7a5dd8697a051afb9170
lapatskiy <val@imastertek.com>
2019-08-21 14:31:54 -0700

    new

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -1,6 +1,7 @@
 package ru.sandbox;
 
 public class MyFirstProgram {
+<<<<<<< HEAD
 
     public static void main(String[] args) {
         hello("world");
@@ -21,5 +22,27 @@ public class MyFirstProgram {
         Square s = new Square(5);
 
         System.out.println("Distance of a square with a side " + s.l + " = " + s.area());
+=======
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Testing if anything has changed");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println(2 + "2");
+
+        System.out.println((2 + 2) * 2);
+
+        System.out.println("2 + 2 = " + (2 + 2));
+
+>>>>>>> parent of cdfb7a0... Added additional functions
     }
 }
