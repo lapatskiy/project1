4120dcdc2e6e6ae2687a8822cd7741c02d3dfb42
lapatskiy <val@imastertek.com>
2019-08-21 13:17:09 -0700

    Revert "Added additional functions"
    This reverts commit cdfb7a0be8fd0b9946d862023aa9531890723a61.

--- sandbox/src/main/java/ru/sandbox/Rectangle.java
+++
@@ -1,6 +0,0 @@
-package ru.sandbox;
-
-public class Rectangle {
-    public double a;
-    public double b;
-}
--- sandbox/src/main/java/ru/sandbox/Square.java
+++
@@ -1,6 +0,0 @@
-package ru.sandbox;
-
-public class Square {
-    public double l;
-
-}
